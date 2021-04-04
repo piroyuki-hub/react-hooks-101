8bff68b35a51cea6340d62c3e5c1cfe47e7a7bcc
piroyuki-hub <sunaga1982+github@gmail.com>
2021-04-04 19:08:45 +0900

    create operation logs

--- src/reducers/operationLogs.js
+++ src/reducers/operationLogs.js
@@ -8,7 +8,7 @@ const operationLogs = (state = [], action) => {
     case ADD_OPERATION_LOG:
       const operationLog = {
         description: action.description,
-        operationAt: action.operationAt
+        operatedAt: action.operatedAt
       }
 
       return [operationLog, ...state]
